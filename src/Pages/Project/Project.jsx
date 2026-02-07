fa594eb97205435a5c8fa683d188951660bcf896
miracle jane <miracleokon2003@gmail.com>
2026-02-07 12:22:26 +0100

    done

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -6,6 +6,9 @@ import 'swiper/css/pagination';
 import check from "../../assets/Pictures/Image2.png"
 import log from "../../assets/Pictures/Image3.png"
 import det from "../../assets/Pictures/Image4.png"
+import tip from "../../assets/Pictures/tip.png"
+import pool from "../../assets/Pictures/pool.png"
+
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 
@@ -30,15 +33,15 @@ const Project = () => {
                 <p className='font-medium mb-2 text-[1.5rem] '>Featured projects</p>
                  <p className='text-[1rem] text-[#dbbfbf]'>Highlighted project showing my frontend development skill</p>
              </div>
-            <div className='  px-5 mt-10  mb-10 lg:px-0 sm:grid grid-cols-2 md:mx-15 rounded-[10px] sm:bg-[#ebe9e5]  md:shadow-xl'>
+            <div className='  px-5 mt-10   mb-10 lg:px-0 sm:grid grid-cols-2  rounded-[10px] sm:bg-[#ebe9e5]  md:shadow-xl'>
                
                   <Swiper
                         spaceBetween={30}
                         centeredSlides={true}
-                        autoplay={{
-                        delay: 1500,
-                        disableOnInteraction: false,
-                        }}
+                        // autoplay={{
+                        // delay: 1500,
+                        // disableOnInteraction: false,
+                        // }}
                         pagination={{
                         clickable: true,
                         }}
@@ -48,23 +51,18 @@ const Project = () => {
                         className=" w-full  "
                     >
                         <SwiperSlide>
-                            <div className='bg-white h-100 pt-10   w-full px-10 sm:px-15 lg:px-40'>
-                                <img src={det} className='h-[90%] w-[100%]' alt="" />
+                            <div className=' h-100 pt-10  sm:px-0 md:px-0   w-full px-10   lg:px-15 xl:px-30'>
+                                <img src={tip} className='h-[90%]  w-[100%]' alt="" />
                             </div>
                             
                         </SwiperSlide>
                         <SwiperSlide>
-                            <div className='bg-white h-100 pt-10  w-full px-10 sm:px-15 lg:px-40'>
-                                <img src={log} className='h-[90%] w-[100%]' alt="" />
+                            <div className='bg-white h-100 pt-10 sm:px-0 md:px-0  w-full px-10  lg:px-15 xl:px-30'>
+                                <img src={pool} className='h-[90%] w-[100%]' alt="" />
                             </div>
                             
                         </SwiperSlide>
-                         <SwiperSlide>
-                            <div className='bg-white h-100 pt-10  w-full  px-10 lg:px-40'>
-                                <img src={check} className='h-[90%] w-[100%]' alt="" />
-                            </div>
-                            
-                        </SwiperSlide>
+                       
                     
                       
                         
@@ -73,12 +71,12 @@ const Project = () => {
                   </Swiper>
                
                 <div className='col-span-1   mt-10 pt-5 px-5 sm:mt-0 bg-[#ebe9e5] text-black  '>
-                   <p className='text-2xl'> Exclusive-Ecommerce </p>
-                    <p className='mt-5'>This project is a responsive e-commerce frontend built with React,
-                         JavaScript, and Tailwind CSS. It focuses on delivering a clean,
-                         intuitive user interface and a smooth shopping experience across all devices. 
-                         The application leverages reusable components and efficient state management to
-                          ensure performance, scalability, and maintainability.
+                   <p className='text-2xl bg-red-500 text-center md:mt-5'> Landing Page Website </p>
+                    <p className='mt-5'>This project is a Landing Page built with React,
+                        JavaScript, and Tailwind CSS. It focuses on delivering a clean,
+                        intuitive user interface and a smooth user experience across all devices. 
+                        The application leverages reusable components and efficient state management to
+                        ensure performance, scalability, and maintainability.
                     </p>
 
                         <div className='  hidden lg:block  md:text-center  md:mt-5 md:text-[1.5rem] font-medium'> <p>Tech Stack</p>
@@ -89,16 +87,19 @@ const Project = () => {
                                 <button className='bg-[#c9b5a0] h-[40px] w-[150px] rounded-[50px]'>Tailwind</button>
                            </div>
                         </div>
-                       <div className=' flex items-center py-5 gap-4'>
+                       <div className=' flex items-center py-5 gap-4 '>
                          <a 
-                            href="https://github.com/Janieel/Exclusive-Ecommerce " >
-                           <button className='mt-10 bg-[#a09a9a] h-12 w-30 rounded-[10px] hover:bg-[#7a7272] '> Github </button>
-                        </a>
-                        <button className='mt-10  bg-[#a09a9a] h-12 w-30 rounded-[10px]  hover:bg-[#7a7272]  '> Live demo </button>
+                            href="https://github.com/Janieel/Tip-Pool-landing-page " >
+                           <button className=' bg-[#a09a9a] h-12 w-30 rounded-[10px] hover:bg-[#7a7272] '> Github </button>
+                        </a>  
+                          <a href="https://tip-pool-page.vercel.app/">
+                        <button className='  bg-[#a09a9a] h-12 w-30 rounded-[10px]  hover:bg-[#7a7272]  '> Live demo </button>
+                       </a> 
                      </div>
                 </div>
               
             </div>
+            
         </div>
     </div>
   )
